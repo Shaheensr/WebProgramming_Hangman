55a1d0424e5db7efca07d05d9fbe5e7ea6513fa3
Shaheen Rahimani <63555317+Shaheensr@users.noreply.github.com>
2020-11-02 20:32:52 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project 2 - Hangman!</title>
+</head>
+<body>
+    <h1>Hangman</h1>
+    <div>
+        <img src="hangman_0.gif">
+        
+    </div>
+    <input type="text">
+    <button type="submit">Submit</button>
+</body>
+</html>
